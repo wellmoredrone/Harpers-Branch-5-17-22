d641f01c0c85170c2c4f629756366d665371bb52
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2022-05-23 17:16:43 -0400

    init

---
+++ data.js
@@ -0,0 +1,215 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-crest",
+      "name": "Crest",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2944,
+      "initialViewParameters": {
+        "yaw": -0.06451202009100498,
+        "pitch": 0.14546297825744503,
+        "fov": 1.3253260922046257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1991916023049107,
+          "pitch": 0.06177549563218143,
+          "rotation": 6.283185307179586,
+          "target": "1-underground-mines"
+        },
+        {
+          "yaw": 2.3484392542343535,
+          "pitch": -0.03015597987075047,
+          "rotation": 6.283185307179586,
+          "target": "2-augering"
+        },
+        {
+          "yaw": 0.2382835220332229,
+          "pitch": -0.010248410144432896,
+          "rotation": 0,
+          "target": "3-face"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-underground-mines",
+      "name": "Underground Mines",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2937.5,
+      "initialViewParameters": {
+        "yaw": -0.028795405239691263,
+        "pitch": 0.2426787098871941,
+        "fov": 1.1514834457970817
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0453116345322382,
+          "pitch": -0.03347723209384412,
+          "rotation": 0,
+          "target": "2-augering"
+        },
+        {
+          "yaw": -1.7391864376369952,
+          "pitch": -0.03628912730893319,
+          "rotation": 0,
+          "target": "0-crest"
+        },
+        {
+          "yaw": -1.609341028766405,
+          "pitch": -0.025109620352194995,
+          "rotation": 0,
+          "target": "3-face"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-augering",
+      "name": "Augering",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2937.5,
+      "initialViewParameters": {
+        "yaw": -0.2762615947429836,
+        "pitch": 0.1975628074214857,
+        "fov": 1.3646202047689777
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8420765775101575,
+          "pitch": -0.04110697366490079,
+          "rotation": 0,
+          "target": "0-crest"
+        },
+        {
+          "yaw": 2.56003965270739,
+          "pitch": -0.023873688528128767,
+          "rotation": 6.283185307179586,
+          "target": "1-underground-mines"
+        },
+        {
+          "yaw": -2.3596717191260392,
+          "pitch": 0.015261728226869664,
+          "rotation": 0,
+          "target": "3-face"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-face",
+      "name": "Face",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2937.5,
+      "initialViewParameters": {
+        "yaw": 0.07652597489513902,
+        "pitch": 0.6720384115212727,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.00002309912902020983,
+          "pitch": -0.2748693608377657,
+          "rotation": 0,
+          "target": "0-crest"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Harper's Branch 5-17-22",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
